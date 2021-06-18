9b930fb7864da9b9aea7cd1977cf87c19018a118
TRỰC NGUYỄN CHÁNH <chanhtruck13@gmail.com.vn>
2021-06-18 11:13:55 +0700

    Initial commit

---
+++ src/main/java/com/kuro/bookStoreV3/BookStoreV3Application.java
@@ -0,0 +1,13 @@
+package com.kuro.bookStoreV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuro/bookStoreV3/BookStoreV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuro.bookStoreV3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
